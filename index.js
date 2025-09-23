cbcbd5e26de847524e138272f45d719e6891b404
kang-teacher <qodl9185@daehyun.hs.kr>
2025-09-23 12:23:06 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/dog', (req, res) => {
+    res.send('<h1>강아지</h1>')
+  })
+
+app.get('/cat', (req, res) => {
+    res.send('고양이')
+  })
+
+app.listen(3000)
