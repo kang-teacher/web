3ef02425eadc74863bb838f91769b2025f1dbf57
kang-teacher <qodl9185@daehyun.hs.kr>
2025-10-01 10:28:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -87,9 +87,7 @@ function unique(arr) {
 }
 
 
-app.listen(3001, () => {
-  console.log('Server listening on http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
-
-
-
