566b05e4bf441541f9ce0c02b37c9e6a07023e2f
kang-teacher <qodl9185@daehyun.hs.kr>
2026-03-19 19:23:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,22 @@ def index():
     }
     return jsonify(response)
 
+@app.route('/2', methods=["GET", "POST"])
+def index2():
+    response = {
+        "version": "2.0",
+        "template": {
+            "outputs": [{
+                "simpleImage": {
+                    "imageUrl":
+                    "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg&quot;,
+                    "altText": "hello I'm Ryan"
+                }
+            }]
+        }
+    }
+    return jsonify(response)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
