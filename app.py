cff4cc8356b2854d635e2d37e699579ac0363463
kang-teacher <qodl9185@daehyun.hs.kr>
2026-03-19 19:27:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def index2():
             "outputs": [{
                 "simpleImage": {
                     "imageUrl":
-                    "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg&quot;,
+                    "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
                     "altText": "hello I'm Ryan"
                 }
             }]
