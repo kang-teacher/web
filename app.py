abddca19bba9ea41e7543ecea8d384d1af4b4215
kang-teacher <qodl9185@daehyun.hs.kr>
2026-03-19 19:28:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
-def index():
-    response = {
+@app.route('/text', methods=["GET", "POST"])
+def text_skill():
+    return jsonify({
         "version": "2.0",
         "template": {
             "outputs": [{
@@ -14,25 +14,21 @@ def index():
                 }
             }]
         }
-    }
-    return jsonify(response)
+    })
 
-@app.route('/2', methods=["GET", "POST"])
-def index2():
-    response = {
+@app.route('/image', methods=["GET", "POST"])
+def image_skill():
+    return jsonify({
         "version": "2.0",
         "template": {
             "outputs": [{
                 "simpleImage": {
-                    "imageUrl":
-                    "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+                    "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
                     "altText": "hello I'm Ryan"
                 }
             }]
         }
-    }
-    return jsonify(response)
-
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
